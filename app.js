99ed382d37f3f5012fcdb69bd0c0ea40c9e42a04
ArtemPalagin <art.palagin@gmail.com>
2022-04-18 13:09:44 +0300

    allow to use MONGODB

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect(process.env.MONGODB || 'mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
