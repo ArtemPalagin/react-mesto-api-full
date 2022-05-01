1e89c778b32a83d88a90eeeab394762bb834a06d
ArtemPalagin <art.palagin@gmail.com>
2022-05-01 15:31:52 +0300

    AuthenticationError

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const AuthenticationError = require('../errors/authentication-error');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -48,13 +49,13 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(email,
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new AuthenticationError('Неправильные почта или пароль'));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new AuthenticationError('Неправильные почта или пароль'));
           }
 
           return user;
